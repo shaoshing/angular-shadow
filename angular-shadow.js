50fc299cc1487e94e040044b1405f296fef619ae
Shaoshing <shaoshing@me.com>
2014-07-05 22:45:30 -0400

    Added layer wrapper to fix z-index error.

--- angular-shadow.js
+++ angular-shadow.js
@@ -19,31 +19,40 @@
           prependShadowStyleToHead();
 
           var divEle = document.createElement('div');
-          divEle.classList.add('ss-bottom-shadow');
-          divEle.classList.add(scope.className);
+          divEle.classList.add('ss-bottom-shadow-layer');
           element.appendChild(divEle);
+
+          var div2Ele = document.createElement('div');
+          div2Ele.classList.add('ss-bottom-shadow');
+          div2Ele.classList.add(scope.className);
+          divEle.appendChild(div2Ele);
         }
       };
     });
 
   var STYLE_ID = 'ss-angular-shadow';
   var STYLE_CONTENT ='\
+    .ss-bottom-shadow-layer{\n\
+      display: block;\
+      position: relative;\
+      width: 100%;\
+      opacity: 0.99;\
+    }\n\
     .ss-bottom-shadow{\n\
-      display:block;\
-      position:relative;\
-      width:100%;\
-      height:5px;\
-      background-color:white;\
+      width: 100%;\
+      height: 100%;\
+      background-color: white;\
+      height: 5px;\
     }\n\
     .ss-bottom-shadow:after {\n\
-      position:absolute;\
-      content:"";\
-      height:1px;\
+      position: absolute;\
+      content: "";\
+      height: 1px;\
       width: calc(100% - 6px);\
-      margin-top:4px;\
-      margin-left:3px;\
-      display:block;\
-      z-index:-1;\
+      margin-top: 4px;\
+      margin-left: 3px;\
+      display: block;\
+      z-index: -1;\
       box-shadow: 0px 0px 3px 1px #ccc;\
     }\n\
   ';
